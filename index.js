b71dfa5ace1d9b90ead9c2ad6de4a4e9f145d94d
mddanishmeta <m.danishtariq12345@gmail.com>
2024-03-28 19:58:52 +0500

    we make number based garde checking tools

---
+++ index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let PercentageNumber = 100;
+const answer = await inquirer.prompt([
+    {
+        name: 'PercentageNumber',
+        type: "number",
+        message: "Plese Enter Your Percentage Number"
+    }
+]);
+if (answer.PercentageNumber >= 80) {
+    console.log("Congratulation You are Passed With A++ Grade");
+}
+else if (answer.PercentageNumber >= 70) {
+    console.log("Congratulation You are Passed With A+ Grade");
+}
+else if (answer.PercentageNumber >= 60) {
+    console.log("Congratulation You are Passed With B Grade");
+}
+else if (answer.PercentageNumber >= 50) {
+    console.log("Congratulation You are Passed With C Grade");
+}
+else if (answer.PercentageNumber <= 49) {
+    console.log("Sorry You are Fail with D Grade");
+}
